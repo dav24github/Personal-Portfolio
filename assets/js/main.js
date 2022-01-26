e4898fb72e89397d28c170e0bea7933f8923b955
dav24github <Davidthbsossa@gmail.com>
2022-01-25 23:41:25 -0400

    language property, blob animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+  navClose = document.getElementById("nav-close"),
+  lang = document.getElementById("nav-menu-lang"),
+  langItems = document.getElementById("nav-menu-langItems"),
+  body = document.querySelector("body");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
@@ -17,6 +20,126 @@ if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
+}
+
+/*===== SHOW LANG =====*/
+if (lang) {
+  lang.addEventListener("click", (e) => {
+    if (!e) e = window.event;
+    e.stopPropagation();
+
+    if (langItems.className === "nav__link-lang lang-open") {
+      langItems.classList.remove("lang-open");
+    } else {
+      langItems.classList.add("lang-open");
+    }
+  });
+}
+/*===== Hiden LANG =====*/
+if (body) {
+  body.addEventListener("click", (e) => {
+    langItems.classList.remove("lang-open");
+  });
+}
+
+/*===== CHANGE LANG =====*/
+/*data from db.js*/
+function myFunction(event) {
+  const attr = event.target.getAttribute("language");
+  //nav
+  const home = document.querySelector("a[href='#home'] span");
+  const about = document.querySelector("a[href='#about'] span");
+  const skills = document.querySelector("a[href='#skills'] span");
+  const education = document.querySelector("a[href='#education'] span");
+  const portfolio = document.querySelector("a[href='#portfolio'] span");
+  const contact = document.querySelector("a[href='#contact'] span");
+  home.textContent = data[attr].nav.home;
+  about.textContent = data[attr].nav.about;
+  skills.textContent = data[attr].nav.skills;
+  education.textContent = data[attr].nav.education;
+  portfolio.textContent = data[attr].nav.portfolio;
+  contact.textContent = data[attr].nav.contact;
+
+  //home
+  const homeTitle = document.querySelector(".home__title");
+  const home__description = document.querySelector(".home__description");
+  const btn_contactme = document.querySelector(
+    ".home__data a[href='#contact']"
+  );
+  const home__scroll = document.querySelector(".home__scroll-name");
+  homeTitle.textContent = data[attr].home.home_title;
+  home__description.textContent = data[attr].home.home__description;
+  btn_contactme.textContent = data[attr].home.btn_contactme;
+  home__scroll.textContent = data[attr].home.home__scroll;
+
+  //About
+  const about__title = document.querySelector(".about .section__title");
+  const about__subtitle = document.querySelector(".about .section__subtitle");
+  const about__description = document.querySelector(
+    ".about .about__description"
+  );
+  const about__button = document.querySelector(".about__buttons a");
+  about__title.textContent = data[attr].about.about__title;
+  about__subtitle.textContent = data[attr].about.section__subtitle;
+  about__description.textContent = data[attr].about.about__description;
+  about__button.textContent = data[attr].about.about__button;
+
+  //Skills
+  const skills__title = document.querySelector(".skills .section__title");
+  const another__tools = document.querySelector(".skills__title.other__skills");
+  skills__title.textContent = data[attr].skills.skills__title;
+  another__tools.textContent = data[attr].skills.another__tools;
+
+  //Qualification
+  const qualification__title = document.querySelector(
+    ".qualification .section__title"
+  );
+  const qualification__title_1 = document.querySelector(
+    ".qualification__title:nth-of-type(1)"
+  );
+  const qualification__subtitle_1 = document.querySelector(
+    ".qualification__title:nth-of-type(1) ~ .qualification__subtitle:nth-of-type(2)"
+  );
+  qualification__title.textContent =
+    data[attr].qualification.qualification__title;
+  qualification__title_1.textContent =
+    data[attr].qualification.qualification__title_1;
+  qualification__subtitle_1.textContent =
+    data[attr].qualification.qualification__subtitle_1;
+
+  //Portfolio
+  const portfolio__title = document.querySelector(".portfolio .section__title");
+  const portfolio__subtitle = document.querySelector(
+    ".portfolio .section__subtitle"
+  );
+  portfolio__title.textContent = data[attr].portfolio.portfolio__title;
+  portfolio__subtitle.textContent = data[attr].portfolio.portfolio__subtitle;
+
+  //Contact
+  const contact__title = document.querySelector(".contact .section__title");
+  const contact__tel = document.querySelector(
+    ".contact__information:nth-of-type(1) .contact__title"
+  );
+  const contact__loc = document.querySelector(
+    ".contact__information:nth-of-type(3) .contact__title"
+  );
+  const contact__button = document.querySelector(".contact__button a");
+  contact__title.textContent = data[attr].contact.contact__title;
+  contact__tel.textContent = data[attr].contact.contact__tel;
+  contact__loc.textContent = data[attr].contact.contact__loc;
+  contact__button.textContent = data[attr].contact.contact__button;
+
+  //Footer
+  const footer__link_portfolio = document.querySelector(
+    ".footer__links a[href='#portfolio']"
+  );
+  const footer__link_contact = document.querySelector(
+    ".footer__links a[href='#contact']"
+  );
+  const footer__subtitle = document.querySelector(".footer__subtitle");
+  footer__link_portfolio.textContent = data[attr].nav.portfolio;
+  footer__link_contact.textContent = data[attr].nav.contact;
+  footer__subtitle.textContent = data[attr].home.home__description;
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
@@ -95,10 +218,10 @@ function scrollHeader() {
 window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
-function scrollTop() {
-  const scrollTop = document.getElementById("scroll-top");
-  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
-  else scrollTop.classList.remove("show-scroll");
-}
-window.addEventListener("scroll", scrollTop);
+// function scrollTop() {
+//   const scrollTop = document.getElementById("scroll-top");
+//   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+//   if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+//   else scrollTop.classList.remove("show-scroll");
+// }
+// window.addEventListener("scroll", scrollTop);
---
+++ db.js
@@ -0,0 +1,86 @@
+var data = {
+  english: {
+    nav: {
+      home: "Home",
+      about: "About",
+      skills: "Skills",
+      education: "Education",
+      portfolio: "Portfolio",
+      contact: "Contactme",
+    },
+    home: {
+      home_title: "Hi, I'am David",
+      home__description: "Web Developer | Full-Stack",
+      btn_contactme: "Contact me",
+      home__scroll: "Scroll down",
+    },
+    about: {
+      about__title: "About Me",
+      section__subtitle: "My introduction",
+      about__description:
+        "I am a software developer with a Bachelor of Computer Science degree  focused mainly on web development (Full-Stack). Looking for new opportunities and highly motivated to learn fast and improve my skills in a team environment.",
+      about__button: "Download CV",
+    },
+    skills: {
+      skills__title: "Skills",
+      another__tools: "Another tools",
+    },
+    qualification: {
+      qualification__title: "Qualification",
+      qualification__title_1: "COMPUTER SCIENCE ENGINEERING",
+      qualification__subtitle_1: "Graduation with Honors",
+    },
+    portfolio: {
+      portfolio__title: "Portfolio",
+      portfolio__subtitle: "Most recent work",
+    },
+    contact: {
+      contact__title: "Contact",
+      contact__tel: "Phone",
+      contact__loc: "Location",
+      contact__button: "Send Message",
+    },
+  },
+  spanish: {
+    nav: {
+      home: "Inicio",
+      about: "Acerca de",
+      skills: "Habilidades",
+      education: "Educación",
+      portfolio: "Portafolio",
+      contact: "Contacto",
+    },
+    home: {
+      home_title: "Hola, soy David",
+      home__description: "Desarrollador web | Full-Stack",
+      btn_contactme: "Contactar",
+      home__scroll: "Has click",
+    },
+    about: {
+      about__title: "Acerca de mi",
+      section__subtitle: "Perfil",
+      about__description:
+        "Soy desarrollador de software con licenciatura en Ciencia de la Computación enfocado pricipalmente en el desarrollo web (Full-Stack). En busca de nuevas oportunidades y altamente motivado para aprender rápido y mejorar mis habilidades en un ambiente de equipo.",
+      about__button: "Descargar CV",
+    },
+    skills: {
+      skills__title: "Habilidades",
+      another__tools: "Otras Herramientas",
+    },
+    qualification: {
+      qualification__title: "Educación",
+      qualification__title_1: "INGENIERÍA EN CIENCIAS DE LA COMPUTACIÓN",
+      qualification__subtitle_1: "Graduación con Excelencia Académica",
+    },
+    portfolio: {
+      portfolio__title: "Portafolio",
+      portfolio__subtitle: "Trabajos más recientes",
+    },
+    contact: {
+      contact__title: "Contacto",
+      contact__tel: "Teléfono",
+      contact__loc: "Ubicación",
+      contact__button: "Enviar Mesaje",
+    },
+  },
+};
